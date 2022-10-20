98915212b71ba16181df99accebec68f7d472852
kevinandres <kevinandres@localhost>
2022-10-20 16:05:35 -0500

    practicaMatrices

---
+++ src/main/java/com/mycompany/poo_practicas_and/Poo_practicas_and.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo_practicas_and;
+
+/**
+ *
+ * @author kevinandres
+ */
+public class Poo_practicas_and {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+    }
+}

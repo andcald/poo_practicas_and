df7d34d4e46a36d7faf94cdb716daac02f012430
kevinandres <kevinandres@localhost>
2022-11-07 20:07:56 -0500

    ejercicio02_01

---
+++ src/main/java/poo_02_01/Atencion.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_02_01;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author kevinandres
+ */
+public class Atencion {
+
+    private LocalDate fecha;
+    private String veterinario;
+    private double costo;
+    private Dueño owner;
+    private Mascota mascota;
+    private int cantidadRecetas;
+    private Receta[] recetaList;
+
+    public Atencion(LocalDate fecha, String veterinario, int cantidadRecetas, Dueño owner, Mascota mascota) {
+        this.fecha = fecha;
+        this.veterinario = veterinario;
+        this.costo = costo;
+        this.owner = owner;
+        this.mascota = mascota;
+        this.cantidadRecetas = cantidadRecetas;
+        this.recetaList = new Receta[this.cantidadRecetas];
+    }
+
+    public void nuevaReceta(String medicamento, int cantidad, String indicacion, int posicion) {
+        var receta = new Receta(medicamento, cantidad, indicacion);
+        this.recetaList[posicion] = receta;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(String veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public Dueño getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Dueño owner) {
+        this.owner = owner;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public int getCantidadRecetas() {
+        return cantidadRecetas;
+    }
+
+    public void setCantidadRecetas(int cantidadRecetas) {
+        this.cantidadRecetas = cantidadRecetas;
+    }
+
+    public Receta[] getRecetaList() {
+        return recetaList;
+    }
+
+    public void setRecetaList(Receta[] recetaList) {
+        this.recetaList = recetaList;
+    }
+
+    @Override
+    public String toString() {
+      
+        System.out.println("La cantidad de recetas es: "+ cantidadRecetas);        
+        for (int i = 0; i < this.cantidadRecetas; i++) {
+            System.out.println(this.recetaList[i]);
+        }
+        return "";
+    }
+
+}

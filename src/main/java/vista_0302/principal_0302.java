915b20626b5695ef6b1e1d82e1485fa95e5de75c
kevinandres <kevinandres@localhost>
2022-11-23 12:03:34 -0500

    ejercicio02_01

---
+++ src/main/java/vista_0302/principal_0302.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista_0302;
+
+/**
+ *
+ * @author kevinandres
+ */
+public class principal_0302 {
+    
+     public static void main(String[] args) {
+        var ventana = new Ventana0302a("Registro de Jugador");
+        
+    }
+    
+}

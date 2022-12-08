047302f6e5aa8947b2357538d86910270465a6cf
kevinandres <kevinandres@localhost>
2022-12-08 16:46:02 -0500

    ejercicio02_01

--- src/main/java/controlador_0303/EmpleadoControl.java
+++ src/main/java/controlador_0303/EmpleadoControl.java
@@ -15,55 +15,37 @@ import vista_0303.VentanaEmpleado;
  * @author kevinandres
  */
 public class EmpleadoControl {
-    
+
     private EmpleadoServiceImpl empleadoServiceImpl;
-    
-    
 
     public EmpleadoControl() {
         this.empleadoServiceImpl = new EmpleadoServiceImpl();
-     
-        
     }
 
-    
-    public String crear(String [] data){
-        var retorno="No se pudo crear el jugador";
-        
-        
-        
-       
-        var nombre=data[0];
-        var codigo=Integer.valueOf(data[1]).intValue();
-        var puesto=data[2];
-        var anio=Integer.valueOf(data[3]).intValue();
-        var mes=Integer.valueOf(data[4]).intValue();
-        var dia=Integer.valueOf(data[5]).intValue();
-        
-        
-        
+    public String crear(String[] data) {
+        var retorno = "No se pudo crear el jugador";
+        var nombre = data[0];
+        var codigo = Integer.valueOf(data[1]).intValue();
+        var puesto = data[2];
+        var anio = Integer.valueOf(data[3]).intValue();
+        var mes = Integer.valueOf(data[4]).intValue();
+        var dia = Integer.valueOf(data[5]).intValue();
+
         if (anio < 1980 && mes > 12 && dia > 31) {
             retorno += " La fecha de nacimiento es incorrecta";
         } else {
             var empleado = new Empleado(nombre, codigo, puesto, LocalDate.of(anio, mes, dia));
             this.empleadoServiceImpl.crear(empleado);
-            
+
             retorno = "Empleado " + empleado.getNombreEmpleado() + " creado correctamente";
         }
         return retorno;
-        
+
     }
     
-    
-    
-    
-    
-    
-    public List<Empleado> listar(){
+
+    public List<Empleado> listar() {
         return this.empleadoServiceImpl.listar();
     }
-    
-    
-    
-    
+
 }
--- src/main/java/vista_0303/VentanaEmpleado.java
+++ src/main/java/vista_0303/VentanaEmpleado.java
@@ -15,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
@@ -29,14 +30,16 @@ import servicio_0303.EmpleadoServiceImpl;
 public class VentanaEmpleado extends JFrame implements ActionListener {
 
     private EmpleadoControl empleadoControl;
-    
+
     private List<JPanel> jPanelList;
     private List<JButton> jButtonList;
     private List<JTextField> jTextFieldList;
     private List<JLabel> jLabelList;
     private List<JTable> jTableList;
     private List<JScrollPane> JScrollList;
-    private List<EmpleadoServiceImpl> empleadosList;
+    private List<EmpleadoServiceImpl> empleadosList; 
+    private List<JTextArea> areaList;
+    
 
     public VentanaEmpleado() {
         this.setSize(600, 600);
@@ -56,11 +59,8 @@ public class VentanaEmpleado extends JFrame implements ActionListener {
         this.jTableList = new ArrayList<>();
         this.JScrollList = new ArrayList<>();
         this.empleadosList = new ArrayList<>();
+        this.areaList = new ArrayList<>();
         
-        
-        
-        
-            
 
         this.jPanelList.add(new JPanel());
         this.jPanelList.get(0).setLayout(null);
@@ -75,7 +75,6 @@ public class VentanaEmpleado extends JFrame implements ActionListener {
         this.jTextFieldList.add(new JTextField("yyyy", 5));
         this.jTextFieldList.add(new JTextField("mm", 5));
         this.jTextFieldList.add(new JTextField("dd", 5));
-        
 
         this.jLabelList.add(new JLabel("Ingrese el nombre del empleado: ", SwingConstants.LEFT));
         this.jLabelList.get(0).setBounds(20, 10, 250, 20);
@@ -113,61 +112,33 @@ public class VentanaEmpleado extends JFrame implements ActionListener {
         this.jPanelList.get(0).add(this.jButtonList.get(1));
         this.jPanelList.get(0).add(this.jButtonList.get(2));
 
-        
         this.jButtonList.get(0).addActionListener(this);
         this.jButtonList.get(1).addActionListener(this);
         this.jButtonList.get(2).addActionListener(this);
-
-        DefaultTableModel modelo = new DefaultTableModel();
-        modelo.addColumn("Nombre");
-        modelo.addColumn("Codigo");
-        modelo.addColumn("Puesto");
-        modelo.addColumn("FechaNacimiento");
-       /*
-        var data = new String[6];
-
-        data[0] = "Andres Calderon";
-        data[1] = "4041";
-        data[2] = "Gerente";
-        data[3] = "19/04/1999";
+        
+         this.jLabelList.add(new JLabel(" ", SwingConstants.LEFT));
+            this.jLabelList.get(4).setBounds(20, 190, 250, 20);
+        this.jPanelList.get(0).add(this.jLabelList.get(4));
+        
+        this.areaList.add(new JTextArea());
+        this.areaList.get(0).setBounds(20, 210, 500, 100);
+        this.areaList.get(0).setText("Texto");
+        
+        this.jPanelList.get(0).add(this.areaList.get(0));
         
         
-*/
-
-        this.jTableList.add(new JTable(modelo));
-        this.jTableList.get(0).setBounds(20, 220, 500, 300);
-        this.jPanelList.get(0).add(this.jTableList.get(0));
-
-        this.JScrollList.add(new JScrollPane(this.jTableList.get(0), ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED));
-        this.JScrollList.get(0).setBounds(20, 220, 500, 300);
-        this.jPanelList.get(0).add(this.JScrollList.get(0));
-
         
-        this.jLabelList.add(new JLabel("Guardado", SwingConstants.LEFT)); 
-        this.jLabelList.get(4).setBounds(20, 190, 250, 20);
-        
-       // this.jTextFieldList.add(new JTextField("dd", 5));
-         
         
     }
-    
-    
-    
-    
-    public void agregarEmpleado(EmpleadoServiceImpl empleadosList1){
-        empleadosList.add(empleadosList1);
-        
-    
-    }
 
-    
-    
-    
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        
+
         if (e.getSource().equals(this.jButtonList.get(0))) {
+            
+           
+            
+            
             String[] datos = new String[6];
             datos[0] = this.jTextFieldList.get(0).getText();
             datos[1] = this.jTextFieldList.get(1).getText();
@@ -175,39 +146,47 @@ public class VentanaEmpleado extends JFrame implements ActionListener {
             datos[3] = this.jTextFieldList.get(3).getText();
             datos[4] = this.jTextFieldList.get(4).getText();
             datos[5] = this.jTextFieldList.get(5).getText();
-            
-            this.jPanelList.get(0).add(this.jLabelList.get(4));
-            
-            
-            
-            
             this.empleadoControl.crear(datos);
-           
             
             
-             
-        }
-
-        
-        
-        if (e.getSource().equals(this.jButtonList.get(1))) {
-            for (var empleado : this.empleadoControl.listar()) {
-                System.out.println("Empleado = " + empleado.toString());
-                
-            }
-        }
-
-        
-        
-        if (e.getSource().equals(this.jButtonList.get(2))) {
-
+            
             this.jTextFieldList.get(0).setText("");
             this.jTextFieldList.get(1).setText("");
             this.jTextFieldList.get(2).setText("");
             this.jTextFieldList.get(3).setText("yyyy");
             this.jTextFieldList.get(4).setText("mm");
             this.jTextFieldList.get(5).setText("dd");
+            
+             this.jLabelList.get(4).setText("Empleado Guardado");
+            
+            
+        }
+        
+        
+        
+        
 
+        if (e.getSource().equals(this.jButtonList.get(1))) {
+            
+            for (var empleado : this.empleadoControl.listar()) {
+                
+               this.areaList.get(0).setText("hola");
+                
+                System.out.println("Empleado = " + empleado.toString());
+
+            }
+            
+            
+            
+        }
+
+        
+        
+        
+        if (e.getSource().equals(this.jButtonList.get(2))) {
+
+            
+ 
         }
 
     }
